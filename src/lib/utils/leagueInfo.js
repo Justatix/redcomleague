60ddd92e5da647060e2513646ef8ace8e5f2f61f
Justatix <95846607+Justatix@users.noreply.github.com>
2024-05-25 21:52:56 +0200

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "918152922595315712"; // your league ID
+export const leagueID = "1049317036473622528"; // your league ID
 export const leagueName = "The RedCom League"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
